ff418bd58b9931aee34b76453350b12c5e5da9f9
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-06 21:06:20 +0200

    preparation for sendGrid to use

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const sendGrid = require('@sendGrid/mail');
+
+const app = express();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+  next();
+});
