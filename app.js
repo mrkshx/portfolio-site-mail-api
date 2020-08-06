f70da01137af9b1d74aea102107a320add3e44d0
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-06 22:16:40 +0200

    sendgrid implemented

--- app.js
+++ app.js
@@ -19,5 +19,29 @@ app.get('/api', (req, res, next) => {
   res.send('API Status: Running')
 });
 
+app.post('api/email', (req, res, next) => {
+  sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: process.env.SENDGRID_EMAIL,
+    from: req.body.email,
+    subject: Portfolio Website Contact,
+    text: req.body.message,
+  }
+
+  sendGrid.send(msg)
+    .then(result => {
+      res.status(200).json({
+        success: true
+      });
+    })
+    .catch(err => {
+      console.log('error: ', err);
+      res.status(401).json({
+        success: false
+      })
+    })
+
+});
+
 app.listen(3030, '0.0.0.0');
 
