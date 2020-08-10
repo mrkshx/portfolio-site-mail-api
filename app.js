f304b40ee011ef5111ca83adb61e72fd248abb3f
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-10 20:12:26 +0200

    fixed sending not working

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const sendGrid = require('@sendgrid/mail');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
 app.get('/api', (req, res, next) => {
   res.send('API Status: Running')
 });
 
-app.post('api/email', (req, res, next) => {
+app.post('/api/email', (req, res, next) => {
   sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: process.env.SENDGRID_EMAIL,
-    from: req.body.email,
-    subject: Portfolio Website Contact,
+    from: `${req.body.name} <${process.env.SENDGRID_EMAIL}>`,
+    replyTo: req.body.email,
+    subject: `Portfolio Website Contact from ${req.body.name}`,
     text: req.body.message,
+    html: `<p>${req.body.message}</p>`,
   }
 
   sendGrid.send(msg)
     .then(result => {
+      console.log('SUCCESS: ', result);
       res.status(200).json({
         success: true
       });
